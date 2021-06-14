8ac6c35d90feb1a195a79f4f178b47ea6bef213c
Cantika Dewi Maharani <66821310+cantika28@users.noreply.github.com>
2021-06-14 09:04:43 +0700

    ApiVolley

---
+++ VolleyAPI/app/src/main/java/com/example/volleyapi/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.volleyapi.Util;
+
+public class ServerAPI {
+        public static final String URL_DATA   = "http://192.168.100.12/Minggu11/Mahasiswa/Api";
+        public static final String URL_INSERT = "http://192.168.100.12/Minggu11/Mahasiswa/ApiInsert";
+        public static final String URL_DELETE = "http://192.168.100.12/Minggu11/Mahasiswa/ApiDelete";
+        public static final String URL_UPDATE = "http://192.168.100.12/Minggu11/Mahasiswa/ApiUpdate";
+    }
